6a9c748a685a32bada7476d86cc211afddf80d15
DewDevth <140059595+DewDevth@users.noreply.github.com>
2023-10-19 17:19:58 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import Hero from "./components/Hero"
+import Navbar from "./components/Navbar"
+
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Hero />
+      {/* <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1> */}
+    </>
+  )
+}
+
+export default App
